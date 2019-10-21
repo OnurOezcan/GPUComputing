929628f27b1b92b33a55b5050a68b588c2ddc899
xOnur <Onur_93@yahoo.de>
2019-10-21 17:29:14 +0200

    von Alex - getMaxMatrixSize

--- Ueb1/main.c
+++ Ueb1/main.c
@@ -9,26 +9,52 @@
 const int ERROR = -1;
 
 
-
-
-void initMemory() 
+unsigned int getMaxMatrixSize() 
 {
-  printf("hier sollte die Ram berechnet werden");
+  long maxSize;
+    int ret;
+    struct sysinfo info;
+    if((ret = sysinfo(&info)) == ERROR){
+        switch(errno){
+            case EACCES : printf("Access denied\n");
+                          return ERROR;
+                          break;
+            default : printf("Some error occured\n");
+                      return ERROR;
+        }
+    }
+    printf("MemTotal           : %11ld bytes\n", info.totalram);
+    printf("MemFree            : %11ld bytes\n", info.freeram);
+    maxSize = info.freeram / sizeof(float);
+    printf("PossibleFloats     : %11ld floats\n", maxSize);
+    //Each Vector needs space, 2 for the nxn-Matrix, one for the input vector and one for the output vector
+    //Therefore use pq-formular because of : n^2+2n=MaximalVerfuegbareFloats
+    unsigned int vectorSize = pow((double)maxSize-0.75F, 1.0F/2.0F)-1;
+    printf("MaxUsableFloats    : %11ld floats\n", (long)pow((double)vectorSize, 2)+2*vectorSize);
+    printf("MaxVectorSize      : %11d indices\n", vectorSize);
+    printf("=================================\n");
+    return vectorSize;
 }
 
 void berechneMatrix()
 {
 
-    printf("hier sollte die Matrix berechnet werden");
+    printf("hier sollte die Matrix multiplikation berechnet werden");
 }
 
+void initMatrix(){
+  printf("hier soll die Matrix und der Vektor zur Multiplikation mit random floats gefüllt werden.");
+}
 
 int main()
 {
 
   //init System max Size (RAM) nXn Matrix + 2 Vektoren (multiplikator und ergebnis)
   // SPEICHERFREIGABE NICHT VERGESSEN!!!!
-  initMemory();
+  getMaxMatrixSize();
+  // fülle Matrix und Vektor mit Random Float werten
+  initMatrix();
+
 
   // start timer
 	struct timeval start, end;
