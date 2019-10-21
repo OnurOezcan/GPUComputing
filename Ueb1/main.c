1ac76de44232674bbb8386af7767429661c3de60
Cedric <cedric.c.schreiner@gmail.com>
2019-10-21 18:36:12 +0200

    Memory Calculation fixed -> mem_unit was ignored in the calculation which resulted in wrong memory sizes

--- Ueb1/main.c
+++ Ueb1/main.c
@@ -1,78 +1,77 @@
 #include <stdio.h>
 #include <sys/time.h>   // for gettimeofday()
-#include <unistd.h> 	  // for sleep()
+#include <unistd.h>      // for sleep()
 #include <sys/sysinfo.h>// for systeminfo
 #include <errno.h>      // for Errornumbers
 #include <math.h>       // for Matrixmultiplikation
-#include <omp.h>        // for multicore process
+//#include <omp.h>        // for multicore process
 
 const int ERROR = -1;
 
 
-unsigned int getMaxMatrixSize() 
-{
-  long maxSize;
-    int ret;
+unsigned int getMaxMatrixSize() {
     struct sysinfo info;
-    if((ret = sysinfo(&info)) == ERROR){
-        switch(errno){
-            case EACCES : printf("Access denied\n");
-                          return ERROR;
-                          break;
-            default : printf("Some error occured\n");
-                      return ERROR;
+    if (sysinfo(&info) == ERROR) {
+        switch (errno) {
+            case EACCES :
+                printf("Access denied\n");
+                return ERROR;
+            default :
+                printf("Some error occured\n");
+                return ERROR;
         }
     }
-    printf("Gesamtspeicher           : %11ld bytes\n", info.totalram);
-    printf("Freier Speicher          : %11ld bytes\n", info.freeram);
-    maxSize = info.freeram / sizeof(float);
-    printf("Anzahl möglicher Floats  : %11ld floats\n", maxSize);
+
+    unsigned long long totalRam = info.mem_unit * info.totalram;
+    unsigned long long freeRam = info.mem_unit * info.freeram;
+    unsigned long long maxSize = freeRam / sizeof(float);
+
+    printf("Gesamtspeicher           : %11llu bytes\n", totalRam);
+    printf("Freier Speicher          : %11llu bytes\n", freeRam);
+    printf("Anzahl möglicher Floats  : %11llu floats\n", maxSize);
     //Each Vector needs space, 2 for the nxn-Matrix, one for the input vector and one for the output vector
     //Therefore use pq-formular because of : n^2+2n=MaximalVerfuegbareFloats
-    unsigned int vectorSize = pow((double)maxSize-0.75F, 1.0F/2.0F)-1;
-    printf("Maximal nutzbare Floats  : %11ld floats\n", (long)pow((double)vectorSize, 2)+2*vectorSize);
+    unsigned int vectorSize = (int)pow((double) maxSize - 0.75F, 1.0F / 2.0F) - 1;
+    printf("Maximal nutzbare Floats  : %11ld floats\n", (long) pow((double) vectorSize, 2) + 2 * vectorSize);
     printf("Maximale Vektor Größe    : %11d indices\n", vectorSize);
     printf("=================================\n");
     return vectorSize;
 }
 
-void berechneMatrix()
-{
+void berechneMatrix() {
 
     printf("hier sollte die Matrix multiplikation berechnet werden");
 }
 
-void initMatrix(unsigned int maxMatrixSize){
-  printf("die hier erstellte Matrix, muss eine %d x %d Matrix sein.", maxMatrixSize, maxMatrixSize);
+void initMatrix(unsigned int maxMatrixSize) {
+    printf("die hier erstellte Matrix, muss eine %d x %d Matrix sein.", maxMatrixSize, maxMatrixSize);
 }
 
-int main()
-{
-
-  //init System max Size (RAM) nXn Matrix + 2 Vektoren (multiplikator und ergebnis)
-  // SPEICHERFREIGABE NICHT VERGESSEN!!!!
-  unsigned int maxMatrixSize = getMaxMatrixSize();
-  // fülle Matrix und Vektor mit Random Float werten
-  initMatrix(maxMatrixSize);
+int main() {
+    //init System max Size (RAM) nXn Matrix + 2 Vektoren (multiplikator und ergebnis)
+    // SPEICHERFREIGABE NICHT VERGESSEN!!!!
+    unsigned int maxMatrixSize = getMaxMatrixSize();
+    // fülle Matrix und Vektor mit Random Float werten
+    initMatrix(maxMatrixSize);
 
 
-  // start timer
-	struct timeval start, end;
+    // start timer
+    struct timeval start, end;
 
-	gettimeofday(&start, NULL);
+    gettimeofday(&start, NULL);
 
-  //Berechne die matrix Multiplikation
-	berechneMatrix();
+    //Berechne die matrix Multiplikation
+    berechneMatrix();
 
-  // end timer
-	gettimeofday(&end, NULL);
+    // end timer
+    gettimeofday(&end, NULL);
 
-  // calculate Op time
-	long seconds = (end.tv_sec - start.tv_sec);
-	long micros = ((seconds * 1e6) + end.tv_usec) - (start.tv_usec);
+    // calculate Op time
+    long seconds = (end.tv_sec - start.tv_sec);
+    long micros = ((seconds * (long)1e6) + end.tv_usec) - (start.tv_usec);
 
-  // print op time
-	printf("Time elpased is %ld seconds and %ld micros\n", seconds, micros);
+    // print op time
+    printf("Time elpased is %ld seconds and %ld micros\n", seconds, micros);
 
-	return 0;
+    return 0;
 }
