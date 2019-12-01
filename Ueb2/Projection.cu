064ba8ee1a475b3714e7942ccb22ed98521aad9c
CedricSchreiner <cedric.c.schreiner@gmail.com>
2019-12-01 21:46:48 +0100

    Uebung 2
    Aufgabe 1 a,b und Aufgabe 2

--- Ueb2/Projection.cu
+++ Ueb2/Projection.cu
@@ -1,53 +1,59 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #include <stdio.h>
 
-__global__ void setThreadIndex(int** deviceMatrix, long matrixSize){
-  int i = blockIdx.x*blockDim.x + threadIdx.x; // index
-  if(i < matrixSize){
-    for(i;i < matrixSize; i++){
-    }
-  }
+// Aufgabe 2
+__global__ void vecafs(float* a, float* b, float* c, int n) {
+	int idx;
+	idx = blockIdx.x + threadIdx.x; //proj(blockIdx,threadIdx.x,...)
+	if (idx < n) {
+		// this can be replaced by the following calculation:
+		c[idx] = a[idx] -((-(idx & 1)) | 1) * b[idx];
+
+		// In order to better explain the calculation, we break it down into its individual steps
+		// 1. Step (id & 1) The bitwise operation gives us a 0 for an even number and a 1 for an odd number.
+		// 2. Step With the minus beofre the first calculation we simply convert the positive 1 into a -1
+		// 3. Step At the moment we get a -1 or 0 as the result, but we want a -1 or 1. The bitwise OR is used for this. The 0 becomes 1 and the -1 remains unchanged.
+		// 4. At the moment we get a 1 for positive numbers and a -1 for negative numbers, but what we want is exactly the opposite. For this reason, with the last minus the whole thing is turned around once again.
+		// A big advantage of the calculation is that all operations are carried out bitwise and the value ranges of the vectors are not restricted. A disadvantage is the multiplication with 1 or -1 at the end with b[idx]. 
+	}
 }
 
-//Aufgabe 2
-__global void vecads(float *a, float *b, float* c, int n){
-  int idx;
-  idx = proj(blockIdx,threadIdx.x,...);
-  if(idx < n){
-    for(int i=n; i < n;i++){
-      for(; idx%2 == 0; i++){
-        c[idx] = a[idx] + b[idx];
-      }
-      for(; idx%2 != 0; i++){
-        c[idx] = a[idx] - b[idx];
-      }
-    }
-  }
+// Aufgabe 1 a)
+__global__ void kernelOddEvenProjection() {
+	const int index = blockIdx.x * blockDim.x + ( threadIdx.x << 1 ) + (blockIdx.x & 1) * (-blockDim.x + 1);
+	printf("Block ID: %d, Block DIM: %d, Thread ID: %d, ID: %d\n", blockIdx.x, blockDim.x, threadIdx.x, index);
 }
 
-int main(void){
-  int width = 4;
-  int heigth = 4;
-  int threadSize = 4;     // B = Threads
-  long matrixSize = (long)width*heigth;
-  long blockSize = matrixSize+(threadSize-1)/threadSize; 
-  int **hostMatrix, **deviceMatrix;
-  
+// Aufgabe 1 b)
+void oddEven() {
+	const int N = 5;
+	const int M = 8;
+	int test[N][M] = { {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0} };
 
-  //allocate host memory
-  hostMatrix = (int**)malloc(matrixSize*sizeof(int));
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < M; x++) {
+			//formel
+			test[y][x] = (x - ((x >= M >> 1)* M >> 1))* (N << 1) + y * 2 + (x >= M >> 1);
 
-  //allocate device memory
-  cudaMalloc(&deviceMatrix, matrixSize*sizeof(int)); 
+			printf("%d ", test[y][x]);
+		}
+		printf("\n");
+	}
+}
 
-  //copy memory from host to device
-  cudaMemcpy(deviceMatrix, hostMatrix, matrixSize*sizeof(int), cudaMemcpyHostToDevice);
+__global__ void firstParallel()
+{
+	printf("Das hier lauft parallel.\n");
+}
 
-  // Perform SAXPY on 1M elements
-  setThreadIndex<<<blockSize,threadSize>>>(deviceMatrix, matrixSize);
+int main() {
+	const int blockDim = 5;
+	const int threadCount = 5;
 
-  //copy memory from device to host
-  cudaMemcpy(hostMatrix, deviceMatrix, matrixSize*sizeof(int), cudaMemcpyDeviceToHost);
-  
-  cudaFree(deviceMatrix);
-  free(hostMatrix);
+	kernelOddEvenProjection <<<blockDim, threadCount >>>();
+	cudaDeviceSynchronize();
+	
+	oddEven();
 }
