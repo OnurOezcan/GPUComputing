4fe9095a37ae116fade8da3d7ed6b9e349430749
CedricSchreiner <cedric.c.schreiner@gmail.com>
2019-12-02 01:11:39 +0100

    Aufgabe 3

--- Ueb2/Projection.cu
+++ Ueb2/Projection.cu
@@ -1,7 +1,13 @@
+
 #include "cuda_runtime.h"
+#include <cuda.h>
+#include <cuda_runtime_api.h>
 #include "device_launch_parameters.h"
+#include <device_functions.h>
 
 #include <stdio.h>
+
+void init();
 
 // Aufgabe 2
 __global__ void vecafs(float* a, float* b, float* c, int n) {
@@ -52,8 +58,114 @@ int main() {
 	const int blockDim = 5;
 	const int threadCount = 5;
 
-	kernelOddEvenProjection <<<blockDim, threadCount >>>();
+	printf("Aufgabe 1 a):\n");
+	kernelOddEvenProjection <<<blockDim, threadCount>>> ();
 	cudaDeviceSynchronize();
-	
+
+	printf("\nAufgabe 1 b):\n");
 	oddEven();
+	printf("\nAufgabe 3:\n");
+	init();
+}
+
+//=========================================================================//
+//                              Aufgabe 3                                  //
+//=========================================================================//
+const unsigned int M = 5;
+const unsigned int N = 4;
+
+__device__ void PrefixSum(int* num, int N) {
+	int i, sum, sumold;
+	sum = sumold = 0;
+	for (i = 0; i < N; i++) {
+		sum += num[i];
+		num[i] = sumold;
+		sumold = sum;
+	}
+}
+
+__global__ void compact(int *a, int *listex, int *listey) {
+	extern __shared__ int num[];
+	int idx = M * threadIdx.x;
+	//init shared memory with 0
+	__syncthreads();
+
+	if (threadIdx.x == 0) {
+		for (int i = 0; i < N; i++) {
+			num[i] = 0;
+		}
+	}
+	__syncthreads();
+
+	for (unsigned int i = 0; i < M; i++) {
+		printf("Thread ID: %d, %d\n", threadIdx.x, a[idx + i]);
+		if (a[idx + i] == 16) {
+			num[threadIdx.x]++;
+		}
+	}
+
+	__syncthreads();
+	
+	if (threadIdx.x == 0) {
+		PrefixSum(num, blockDim.x);
+	}
+
+	__syncthreads();
+
+	int tmp = 0;
+	for (unsigned int i = 0; i < M; i++) {
+		if (a[idx + i] == 16) {
+			listex[num[threadIdx.x] + tmp] = threadIdx.x;
+			listey[num[threadIdx.x] + tmp] = i;
+			tmp++;
+		}
+	}
+	__syncthreads();
+	if (threadIdx.x == 0) {
+		for (int i = 0; i < N * M; i++) {
+			printf("%d, %d\n", listex[i], listey[i]);
+		}
+	}
+	__syncthreads();
+}
+
+void init() {
+	int* listX;
+	int* listY;
+
+	int* mandelBrot;
+	cudaMallocManaged(&mandelBrot, M * N * sizeof(int));
+	cudaMallocManaged(&listX, M * sizeof(int));
+	cudaMallocManaged(&listY, M * sizeof(int));
+
+	//simulated calculation of the mandelbrot 
+	mandelBrot[0] = 0;
+	mandelBrot[1] = 4;
+	mandelBrot[2] = 16;
+	mandelBrot[3] = 8;
+	mandelBrot[4] = 13;
+
+	mandelBrot[5] = 4;
+	mandelBrot[6] = 16;
+	mandelBrot[7] = 16;
+	mandelBrot[8] = 3;
+	mandelBrot[9] = 14;
+
+	mandelBrot[10] = 9;
+	mandelBrot[11] = 12;
+	mandelBrot[12] = 16;
+	mandelBrot[13] = 5;
+	mandelBrot[14] = 16;
+
+	mandelBrot[15] = 1;
+	mandelBrot[16] = 7;
+	mandelBrot[17] = 15;
+	mandelBrot[18] = 16;
+	mandelBrot[19] = 14;
+
+	compact << <1, N, N * sizeof(int)>> > (mandelBrot, listX, listY);
+
+	cudaFree(mandelBrot);
+	cudaFree(listX);
+	cudaFree(listY);
 }
